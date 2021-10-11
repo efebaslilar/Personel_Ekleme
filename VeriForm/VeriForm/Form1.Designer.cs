5723469eab8a4d64835c00dce66ebcff322829a6
efebaslilar <92309180+efebaslilar@users.noreply.github.com>
2021-10-11 16:39:07 +0300

    Initial commit

---
+++ VeriForm/VeriForm/Form1.Designer.cs
@@ -0,0 +1,271 @@
+﻿namespace VeriForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.gridPersonel = new System.Windows.Forms.DataGridView();
+            this.txtArama = new System.Windows.Forms.TextBox();
+            this.txtPersonelNo = new System.Windows.Forms.TextBox();
+            this.txtPersonelAdi = new System.Windows.Forms.TextBox();
+            this.txtPersonelSoyadi = new System.Windows.Forms.TextBox();
+            this.cmbCinsiyet = new System.Windows.Forms.ComboBox();
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.btnGuncelle = new System.Windows.Forms.Button();
+            this.btnSil = new System.Windows.Forms.Button();
+            this.txtMaas = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.gridPersonel)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.label1.Location = new System.Drawing.Point(25, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "İsme Göre Arama";
+            // 
+            // gridPersonel
+            // 
+            this.gridPersonel.AllowUserToAddRows = false;
+            this.gridPersonel.AllowUserToDeleteRows = false;
+            this.gridPersonel.AllowUserToResizeColumns = false;
+            this.gridPersonel.AllowUserToResizeRows = false;
+            this.gridPersonel.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.gridPersonel.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gridPersonel.Cursor = System.Windows.Forms.Cursors.Default;
+            this.gridPersonel.Location = new System.Drawing.Point(12, 51);
+            this.gridPersonel.MultiSelect = false;
+            this.gridPersonel.Name = "gridPersonel";
+            this.gridPersonel.ReadOnly = true;
+            this.gridPersonel.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.gridPersonel.Size = new System.Drawing.Size(555, 243);
+            this.gridPersonel.TabIndex = 1;
+            this.gridPersonel.TabStop = false;
+            this.gridPersonel.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.gridPersonel_CellClick);
+            this.gridPersonel.DataBindingComplete += new System.Windows.Forms.DataGridViewBindingCompleteEventHandler(this.gridPersonel_DataBindingComplete);
+            // 
+            // txtArama
+            // 
+            this.txtArama.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txtArama.Location = new System.Drawing.Point(122, 9);
+            this.txtArama.Name = "txtArama";
+            this.txtArama.Size = new System.Drawing.Size(100, 20);
+            this.txtArama.TabIndex = 2;
+            this.txtArama.TextChanged += new System.EventHandler(this.txtArama_TextChanged);
+            // 
+            // txtPersonelNo
+            // 
+            this.txtPersonelNo.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txtPersonelNo.Location = new System.Drawing.Point(122, 300);
+            this.txtPersonelNo.Name = "txtPersonelNo";
+            this.txtPersonelNo.Size = new System.Drawing.Size(134, 20);
+            this.txtPersonelNo.TabIndex = 3;
+            this.txtPersonelNo.TextChanged += new System.EventHandler(this.txtPersonelNo_TextChanged);
+            // 
+            // txtPersonelAdi
+            // 
+            this.txtPersonelAdi.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txtPersonelAdi.Location = new System.Drawing.Point(122, 326);
+            this.txtPersonelAdi.Name = "txtPersonelAdi";
+            this.txtPersonelAdi.Size = new System.Drawing.Size(134, 20);
+            this.txtPersonelAdi.TabIndex = 4;
+            // 
+            // txtPersonelSoyadi
+            // 
+            this.txtPersonelSoyadi.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txtPersonelSoyadi.Location = new System.Drawing.Point(122, 352);
+            this.txtPersonelSoyadi.Name = "txtPersonelSoyadi";
+            this.txtPersonelSoyadi.Size = new System.Drawing.Size(134, 20);
+            this.txtPersonelSoyadi.TabIndex = 5;
+            // 
+            // cmbCinsiyet
+            // 
+            this.cmbCinsiyet.Cursor = System.Windows.Forms.Cursors.Default;
+            this.cmbCinsiyet.FormattingEnabled = true;
+            this.cmbCinsiyet.Location = new System.Drawing.Point(122, 378);
+            this.cmbCinsiyet.Name = "cmbCinsiyet";
+            this.cmbCinsiyet.Size = new System.Drawing.Size(67, 21);
+            this.cmbCinsiyet.TabIndex = 6;
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.Cursor = System.Windows.Forms.Cursors.Default;
+            this.btnEkle.Location = new System.Drawing.Point(334, 300);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(75, 33);
+            this.btnEkle.TabIndex = 7;
+            this.btnEkle.Text = "Ekle";
+            this.btnEkle.UseVisualStyleBackColor = true;
+            this.btnEkle.Click += new System.EventHandler(this.btnEkle_Click);
+            // 
+            // btnGuncelle
+            // 
+            this.btnGuncelle.Cursor = System.Windows.Forms.Cursors.Default;
+            this.btnGuncelle.Location = new System.Drawing.Point(334, 340);
+            this.btnGuncelle.Name = "btnGuncelle";
+            this.btnGuncelle.Size = new System.Drawing.Size(75, 32);
+            this.btnGuncelle.TabIndex = 8;
+            this.btnGuncelle.Text = "Güncelle";
+            this.btnGuncelle.UseVisualStyleBackColor = true;
+            this.btnGuncelle.Click += new System.EventHandler(this.btnGuncelle_Click);
+            // 
+            // btnSil
+            // 
+            this.btnSil.Cursor = System.Windows.Forms.Cursors.Default;
+            this.btnSil.Location = new System.Drawing.Point(334, 378);
+            this.btnSil.Name = "btnSil";
+            this.btnSil.Size = new System.Drawing.Size(75, 34);
+            this.btnSil.TabIndex = 9;
+            this.btnSil.Text = "Sil";
+            this.btnSil.UseVisualStyleBackColor = true;
+            this.btnSil.Click += new System.EventHandler(this.btnSil_Click);
+            // 
+            // txtMaas
+            // 
+            this.txtMaas.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txtMaas.Location = new System.Drawing.Point(122, 405);
+            this.txtMaas.Name = "txtMaas";
+            this.txtMaas.Size = new System.Drawing.Size(134, 20);
+            this.txtMaas.TabIndex = 10;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Cursor = System.Windows.Forms.Cursors.Default;
+            this.label2.Location = new System.Drawing.Point(42, 307);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Personel No";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Cursor = System.Windows.Forms.Cursors.Default;
+            this.label3.Location = new System.Drawing.Point(42, 333);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(20, 13);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Ad";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Cursor = System.Windows.Forms.Cursors.Default;
+            this.label4.Location = new System.Drawing.Point(42, 359);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(37, 13);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Soyad";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Cursor = System.Windows.Forms.Cursors.Default;
+            this.label5.Location = new System.Drawing.Point(42, 386);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(43, 13);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Cinsiyet";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Cursor = System.Windows.Forms.Cursors.Default;
+            this.label6.Location = new System.Drawing.Point(42, 411);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(33, 13);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "Maaş";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(599, 437);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtMaas);
+            this.Controls.Add(this.btnSil);
+            this.Controls.Add(this.btnGuncelle);
+            this.Controls.Add(this.btnEkle);
+            this.Controls.Add(this.cmbCinsiyet);
+            this.Controls.Add(this.txtPersonelSoyadi);
+            this.Controls.Add(this.txtPersonelAdi);
+            this.Controls.Add(this.txtPersonelNo);
+            this.Controls.Add(this.txtArama);
+            this.Controls.Add(this.gridPersonel);
+            this.Controls.Add(this.label1);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(615, 476);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(615, 476);
+            this.Name = "Form1";
+            this.Text = "Berkay Efe Başlılar";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.gridPersonel)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView gridPersonel;
+        private System.Windows.Forms.TextBox txtArama;
+        private System.Windows.Forms.TextBox txtPersonelNo;
+        private System.Windows.Forms.TextBox txtPersonelAdi;
+        private System.Windows.Forms.TextBox txtPersonelSoyadi;
+        private System.Windows.Forms.ComboBox cmbCinsiyet;
+        private System.Windows.Forms.Button btnEkle;
+        private System.Windows.Forms.Button btnGuncelle;
+        private System.Windows.Forms.Button btnSil;
+        private System.Windows.Forms.TextBox txtMaas;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ VeriForm/VeriForm/Form1.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data;
+using System.Data.OleDb;
+
+namespace VeriForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        OleDbConnection baglanti = new OleDbConnection("Provider=Microsoft.ACE.Oledb.12.0; Data Source=data.accdb");
+        OleDbDataAdapter adapter;
+        OleDbDataReader reader;
+        OleDbCommand komut;
+        DataSet set;
+        string personelId;
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cmbCinsiyet.Items.Add("Erkek");
+            cmbCinsiyet.Items.Add("Kadın");
+            gridDoldur();
+        }
+
+        public void gridDoldur() 
+        {
+            try
+            {
+                baglanti.Close();
+                adapter = new OleDbDataAdapter("SELECT * FROM Personel", baglanti);
+                set = new DataSet();
+                baglanti.Open();
+                adapter.Fill(set, "Personel");
+                gridPersonel.DataSource = set.Tables["Personel"];
+                baglanti.Close();
+
+                gridPersonel.Columns[0].Visible = false;
+                gridPersonel.Columns[1].HeaderText = "Personel No";
+                gridPersonel.Columns[2].HeaderText = "Ad";
+                gridPersonel.Columns[3].HeaderText = "Soyad";
+                gridPersonel.Columns[4].HeaderText = "Cinsiyet";
+                gridPersonel.Columns[5].HeaderText = "Maaş";
+
+                this.gridPersonel.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            }
+            catch (Exception)
+            {
+            }
+           
+        }
+
+        private void txtPersonelNo_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        
+        private void btnEkle_Click(object sender, EventArgs e) 
+        {
+            komut = new OleDbCommand();
+            baglanti.Open();
+            komut.Connection = baglanti;
+            string Query = "INSERT INTO Personel (personelNo,personelAdi,personelSoyadi,personelCinsiyet,personelMaas) values (?,?,?,?,?);";
+            komut = new OleDbCommand(Query, baglanti);
+            komut.Parameters.Add("@personelNo", OleDbType.VarChar).Value = txtPersonelNo.Text;
+            komut.Parameters.Add("@personelAd", OleDbType.VarChar).Value = txtPersonelAdi.Text;
+            komut.Parameters.Add("@personelSoyadi", OleDbType.VarChar).Value = txtPersonelSoyadi.Text;
+            komut.Parameters.Add("@personelCinsiyet", OleDbType.VarChar).Value = cmbCinsiyet.SelectedItem.ToString();
+            komut.Parameters.Add("@personelMaas", OleDbType.VarChar).Value = txtMaas.Text;
+            reader = komut.ExecuteReader();           
+            baglanti.Close();
+            gridDoldur();
+
+            txtPersonelNo.Text = "";
+            txtPersonelAdi.Text = "";
+            txtPersonelSoyadi.Text = "";
+            txtMaas.Text = "";
+            cmbCinsiyet.Text = "";
+        }
+
+        private void btnGuncelle_Click(object sender, EventArgs e)
+        {
+            komut = new OleDbCommand();
+            baglanti.Open();
+            komut.Connection = baglanti;
+            string Query = "UPDATE PERSONEL SET personelNo=?,personelAdi=?,personelSoyadi=?,personelCinsiyet=?,personelMaas=? WHERE id=?";
+            komut = new OleDbCommand(Query, baglanti);
+            komut.Parameters.Add("@personelNo", OleDbType.VarChar).Value = txtPersonelNo.Text;
+            komut.Parameters.Add("@personelAd", OleDbType.VarChar).Value = txtPersonelAdi.Text;
+            komut.Parameters.Add("@personelSoyadi", OleDbType.VarChar).Value = txtPersonelSoyadi.Text;
+            komut.Parameters.Add("@personelCinsiyet", OleDbType.VarChar).Value = cmbCinsiyet.SelectedItem.ToString();
+            komut.Parameters.Add("@personelMaas", OleDbType.VarChar).Value = txtMaas.Text;
+            komut.Parameters.Add("@id", OleDbType.Integer).Value = personelId;
+            reader = komut.ExecuteReader();
+            baglanti.Close();
+            gridDoldur();
+
+            personelId = "";
+            txtPersonelNo.Text = "";
+            txtPersonelAdi.Text = "";
+            txtPersonelSoyadi.Text = "";
+            txtMaas.Text = "";
+            cmbCinsiyet.Text = "";
+        }
+
+        private void btnSil_Click(object sender, EventArgs e)
+        {
+            komut = new OleDbCommand();
+            baglanti.Open();
+            komut.Connection = baglanti;
+            string Query = "DELETE FROM PERSONEL WHERE id=?";
+            komut = new OleDbCommand(Query, baglanti);
+            komut.Parameters.Add("@id", OleDbType.Integer).Value = personelId;
+            komut.ExecuteNonQuery();
+            baglanti.Close();
+            gridDoldur();
+            personelId = "";
+        }
+
+        private void txtArama_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                gridPersonel.DataSource = "";
+                set.Tables.Clear();
+                baglanti.Close();
+                adapter = new OleDbDataAdapter("SELECT * FROM Personel WHERE personelAdi LIKE '" + txtArama.Text + "%' ", baglanti);
+                set = new DataSet();
+                baglanti.Open();
+                adapter.Fill(set, "Personel");
+                gridPersonel.DataSource = set.Tables["Personel"];
+                baglanti.Close();
+
+                gridPersonel.Columns[0].Visible = false;
+                gridPersonel.Columns[1].HeaderText = "Personel No";
+                gridPersonel.Columns[2].HeaderText = "Ad";
+                gridPersonel.Columns[3].HeaderText = "Soyad";
+                gridPersonel.Columns[4].HeaderText = "Cinsiyet";
+                gridPersonel.Columns[5].HeaderText = "Maaş";
+
+                this.gridPersonel.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            }
+            catch (Exception)
+            {
+            }
+        }
+
+        private void gridPersonel_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            try
+            {
+                cmbCinsiyet.Text = "";
+                personelId = gridPersonel.SelectedRows[0].Cells[0].Value.ToString();
+                txtPersonelNo.Text = gridPersonel.SelectedRows[0].Cells[1].Value.ToString();
+                txtPersonelAdi.Text = gridPersonel.SelectedRows[0].Cells[2].Value.ToString();
+                txtPersonelSoyadi.Text = gridPersonel.SelectedRows[0].Cells[3].Value.ToString();
+                cmbCinsiyet.Text = gridPersonel.SelectedRows[0].Cells[4].Value.ToString();
+                txtMaas.Text = gridPersonel.SelectedRows[0].Cells[5].Value.ToString();
+
+            }
+            catch
+            {
+
+            }
+        }
+
+        private void gridPersonel_DataBindingComplete(object sender, DataGridViewBindingCompleteEventArgs e)
+        {
+            gridPersonel.ClearSelection();
+        }
+    }
+}
